10765c961aa46b90f29f8350d53f856c2c0479f3
José Ricardo Ventrilho <ricardoventrilho@gmail.com>
2017-12-18 18:07:04 -0200

    Alterações do sintático

--- src/negocio/analisadorsintaticoportugol.cpp
+++ src/negocio/analisadorsintaticoportugol.cpp
@@ -2,10 +2,34 @@
 
 negocio::AnalisadorSintaticoPortugol::AnalisadorSintaticoPortugol()
 {
-
+    regras =
+    {
+        { EnumToken::VARIAVEL, EnumToken::OPERADOR_ATRIBUICAO, EnumToken::NUMERO }
+    };
 }
 
 void negocio::AnalisadorSintaticoPortugol::valideSequenciaDeTokens(vector<negocio::TokenPortugol *> tokens)
 {
-    ////TODO: Desenvolver
+    vector<EnumToken> sequenciaDeTokens;
+
+    for (auto token : tokens)
+    {
+        sequenciaDeTokens.push_back(token->getTipo());
+    }
+
+    for (auto regra : regras)
+    {
+        ////Validar token a token
+        if (regra == sequenciaDeTokens)
+        {
+            return;
+        }
+    }
+
+    throw new Excecao("Erro sintático!");
+}
+
+bool negocio::AnalisadorSintaticoPortugol::valideRegraComSequenciaDeTokens(vector<enumeradores::EnumToken> regra, vector<enumeradores::EnumToken> tokens)
+{
+    return false;
 }
--- src/negocio/analisadorsintaticoportugol.h
+++ src/negocio/analisadorsintaticoportugol.h
@@ -2,9 +2,13 @@
 #define ANALISADORSEMANTICO_H
 
 #include <vector>
+#include <map>
 #include "tokenportugol.h"
+#include "infraestrutura/utilitarios/excecao.h"
 
 using std::vector;
+using std::map;
+using infraestrutura::Excecao;
 
 namespace negocio
 {
@@ -13,6 +17,10 @@ class AnalisadorSintaticoPortugol
 public:
     AnalisadorSintaticoPortugol();
     void valideSequenciaDeTokens(vector<TokenPortugol*> tokens);
+
+private:
+    bool valideRegraComSequenciaDeTokens(vector<EnumToken> regra, vector<EnumToken> tokens);
+    vector<vector<EnumToken>> regras;
 };
 }
 
