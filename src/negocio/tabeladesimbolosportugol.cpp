058e024c5c3d99dff0d7c783c5e20eaa2c902918
kaliston <kaliston.silvanio@gmail.com>
2017-12-18 22:49:33 -0200

    requisito 1 - a

--- src/negocio/tabeladesimbolosportugol.cpp
+++ src/negocio/tabeladesimbolosportugol.cpp
@@ -4,20 +4,20 @@ negocio::TabelaDeSimbolosPortugol::TabelaDeSimbolosPortugol()
 {
     tabela =
     {
-        TokenPortugol(QString("programa"), EnumToken::PALAVRA_RESERVADA),
-        TokenPortugol(QString("var"), EnumToken::VARIAVEL,""),
-        TokenPortugol(QString("="), EnumToken::OPERADOR_ATRIBUICAO,""),
-        TokenPortugol(QString(","), EnumToken::OPERADOR_SEPARADOR,""),
-        TokenPortugol(QString("[0-9]{0,30}"), EnumToken::NUMERO,""),
-        TokenPortugol(QString("^(_|[a-Z])([a-Z]|[0-9])*$"), EnumToken::NOME_VARIAVEL,""),
-        TokenPortugol(QString("se"), EnumToken::OPERADOR_SE_LOGICO,""),
-        TokenPortugol(QString("senao"), EnumToken::OPERADOR_SENAO_LOGICO,""),
-        TokenPortugol(QString("(>|<|>=|<=|!=)"), EnumToken::OPERADOR_RELACIONAL,""),
+        TokenPortugol(QString("programa"), EnumToken::PALAVRA_RESERVADA, "#include<stdio.h>_int main()_{_cleanCode_return 0;_}"),
+        TokenPortugol(QString("var"), EnumToken::VARIAVEL,"int"),
+        TokenPortugol(QString("="), EnumToken::OPERADOR_ATRIBUICAO,"="),
+        TokenPortugol(QString(","), EnumToken::OPERADOR_SEPARADOR,","),
+        TokenPortugol(QString("[0-9]{0,30}"), EnumToken::NUMERO,"numero"),
+        TokenPortugol(QString("^(_|[a-Z])([a-Z]|[0-9])*$"), EnumToken::NOME_VARIAVEL,"nome"),
+        TokenPortugol(QString("se"), EnumToken::OPERADOR_SE_LOGICO,"if"),
+        TokenPortugol(QString("senao"), EnumToken::OPERADOR_SENAO_LOGICO,"else"),
+        TokenPortugol(QString("(>|<|>=|<=|!=)"), EnumToken::OPERADOR_RELACIONAL,"relacional"),
         TokenPortugol(QString("leia"), EnumToken::OPERADOR_ARITMETICO,""),
-        TokenPortugol(QString("escreva"), EnumToken::ESCREVA,""),
-        TokenPortugol(QString("("), EnumToken::ABRE,""),
-        TokenPortugol(QString(")"), EnumToken::FECHA,""),
-        TokenPortugol(QString("(+|-|*)"), EnumToken::OPERADOR_ARITMETICO,"")
+        TokenPortugol(QString("escreva"), EnumToken::ESCREVA,"printf"),
+        TokenPortugol(QString("("), EnumToken::ABRE,"("),
+        TokenPortugol(QString(")"), EnumToken::FECHA,")"),
+        TokenPortugol(QString("(+|-|*)"), EnumToken::OPERADOR_ARITMETICO,"operador")
     };
 }
 
